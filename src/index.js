d7d00005282040630d11dce3b8da9561e0cbd358
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-01-07 11:57:16 +0530

    Frist Commit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/> 
+        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Karla:wght@700&display=swap" rel="stylesheet"/>
+        <link rel="stylesheet" href="style.css"/>
   </React.StrictMode>
 );
 
